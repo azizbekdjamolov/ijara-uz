e057f7b10a1b21aaa2f384744b54cd4de9f008ed
meyliyev72 <muhammadmeyliyev35@gmail.com>
2026-08-19 15:14:59 +0500

    serve /media/ in production for demo uploads

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve as media_serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.api.urls")),
+    path("media/<path:path>", media_serve, {"document_root": settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
